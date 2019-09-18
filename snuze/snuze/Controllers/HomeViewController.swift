83d966e913acc5142f69a93bb6ad52fca0522f5d
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-18 11:50:37 -0600

    feat(homeviewcontroller): add initial set alarm button

--- snuze/snuze/Controllers/HomeViewController.swift
+++ snuze/snuze/Controllers/HomeViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    let alarmButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("set alarm", for: .normal)
+        button.backgroundColor = .cyan
+        button.layer.cornerRadius = 15
+        button.tintColor = .blue
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        
+        setupNavBar()
+        
+        view.addSubview(alarmButton)
+        
+        
+        view.addConstraintsWithFormat(format: "V:[v0(50)]-200-|", views: alarmButton)
+        view.addConstraintsWithFormat(format: "H:|-100-[v0]-100-|", views: alarmButton)
+        
     }
-    
+}
 
+//MARK: Helper Methods
+extension HomeViewController {
+    
+    fileprivate func setupNavBar() {
+        if let navBar = navigationController?.navigationBar {
+            navBar.isHidden = true
+        }
+    }
 }
 
---
+++ snuze/snuze/Views/CustomTimePicker.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTimePicker.swift
+//  snuze
+//
+//  Created by Scott Clampet on 9/18/19.
+//  Copyright © 2019 Tao Team. All rights reserved.
+//
+
+import UIKit
+
+class CustomTimePicker: UIPickerView {
+
+    
+
+}
